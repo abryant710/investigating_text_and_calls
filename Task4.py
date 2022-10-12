2856f6e146c0dba7e049ee16effda8912d63b278
Alex Bryant <alexbryant710@gmail.com>
2022-10-12 19:57:40 +1100

    Fixing filter to look for 140 format for telemarketers on final task

--- Task4.py
+++ Task4.py
@@ -32,7 +32,9 @@ def get_non_telemarketers():
 def get_possible_telemarketers():
     non_telemarketers = get_non_telemarketers()
     all_out_calls = calls["call_inc_num"].tolist()
-    filtered = filter(lambda num: num not in non_telemarketers, all_out_calls)
+    filtered = filter(
+        lambda num: num[0:3] == "140" and num not in non_telemarketers, all_out_calls
+    )
     return sorted(list(set(filtered)))
 
 
