289cb0fff57e6f940c3f42834e16135fda506da7
Alex Bryant <alexbryant710@gmail.com>
2022-10-12 19:46:39 +1100

    Finished final task

--- Task4.py
+++ Task4.py
@@ -2,15 +2,12 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
-import csv
+import pandas as pd
 
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+text_col_names = ["text_inc_num", "text_ans_num", "text_time"]
+texts = pd.read_csv("texts.csv", names=text_col_names)
+call_col_names = ["call_inc_num", "call_ans_num", "call_time", "call_len_s"]
+calls = pd.read_csv("calls.csv", names=call_col_names)
 
 """
 TASK 4:
@@ -25,3 +22,24 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+def get_non_telemarketers():
+    all_texts = texts["text_inc_num"].tolist() + texts["text_ans_num"].tolist()
+    all_in_calls = calls["call_ans_num"].tolist()
+    return list(set(all_texts + all_in_calls))
+
+
+def get_possible_telemarketers():
+    non_telemarketers = get_non_telemarketers()
+    all_out_calls = calls["call_inc_num"].tolist()
+    filtered = filter(lambda num: num not in non_telemarketers, all_out_calls)
+    return sorted(list(set(filtered)))
+
+
+def print_possible_telemarketers():
+    print("These numbers could be telemarketers: ")
+    for num in get_possible_telemarketers():
+        print(num)
+
+
+print_possible_telemarketers()
